380b4e6e57bc6a2b1230931dc9773304533b0ff2
rubinoAM <rubinoAM@users.noreply.github.com>
2018-11-27 15:36:40 -0500

    Look_up_record() Introduced

--- jukeboxapp.py
+++ jukeboxapp.py
@@ -1,16 +1,8 @@
+#Runs most optimally in Python 3.7
 from jukebox import Jukebox
 from record import *
 import os
 mom_and_pops_jukebox = Jukebox()
 
 mom_and_pops_jukebox.initialize_box()
-
-print mom_and_pops_jukebox.inventory['A'][5]
-print mom_and_pops_jukebox.inventory['A'][5]['genre']
-
-for k, v in mom_and_pops_jukebox.inventory.items():
-    for array in v:
-        for k, v in array.items():
-            if k=="year":
-                if v in range(1990,2000):
-                    print ("%s by %s: The B-Side on this record is %s." % (array['aside'],array['artist'],array['bside']))
+mom_and_pops_jukebox.look_up_record()
