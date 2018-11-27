506f45fb9941d9128743596bc098bd44f7d28098
rubinoAM <rubinoAM@users.noreply.github.com>
2018-11-27 14:19:17 -0500

    Cleaned up

--- jukeboxapp.py
+++ jukeboxapp.py
@@ -1,7 +1,16 @@
 from jukebox import Jukebox
+from record import *
+import os
 mom_and_pops_jukebox = Jukebox()
 
-mom_and_pops_jukebox.add_record(5,0)
-print (mom_and_pops_jukebox.inventory[0])
+mom_and_pops_jukebox.initialize_box()
 
-mom_and_pops_jukebox.del_record(5)
+print mom_and_pops_jukebox.inventory['A'][5]
+print mom_and_pops_jukebox.inventory['A'][5]['genre']
+
+for k, v in mom_and_pops_jukebox.inventory.items():
+    for array in v:
+        for k, v in array.items():
+            if k=="year":
+                if v in range(1990,2000):
+                    print ("%s by %s: The B-Side on this record is %s." % (array['aside'],array['artist'],array['bside']))
