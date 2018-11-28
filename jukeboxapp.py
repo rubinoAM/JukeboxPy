853914676c5b3dca2018d2cfeafbdbcf0c7cce86
rubinoAM <rubinoAM@users.noreply.github.com>
2018-11-28 09:40:02 -0500

    First attempt at continuous interaction

--- jukeboxapp.py
+++ jukeboxapp.py
@@ -1,8 +1,9 @@
 #Runs most optimally in Python 3.7
-from jukebox import Jukebox
+from jukebox import Jukebox, main_menu
 from record import *
 import os
 mom_and_pops_jukebox = Jukebox()
-
 mom_and_pops_jukebox.initialize_box()
-mom_and_pops_jukebox.look_up_record()
+
+if (main_menu):
+    mom_and_pops_jukebox.start_up()
